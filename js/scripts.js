3f162ca5c4c3c9ac1fbeb82c064a1b2e5c20e161
masha <mashanechmo@gmail.com>
2021-06-08 14:30:00 -0700

    add bold passage function
    Co-authored-by: Allister <allisterkmoon@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+// UI Logic
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+  });
+});
 // Business Logic
 
 function wordCounter(text) {
@@ -33,5 +46,24 @@ function numberOfOccurrencesInText(word, text) {
     }
   })
 
-  return wordCount
+  return wordCount;
+}
+
+function boldPassage (word, text) {
+  let htmlString = '<p>'
+  let textArray = text.split(' ')
+
+  textArray.forEach(function (element, index) {
+    if (word === element) {
+      htmlString = htmlString.concat('<b>' + element + '</b>')
+    } else {
+      htmlString = htmlString.concat(element)
+    }
+
+    if (index !== textArray.length - 1) {
+      htmlString = htmlString.concat(' ')
+    }
+  })
+
+  return htmlString + '</p>'
 }
